95a05432984957938c811ef4e737a557c8e7ce00
ngumcaleb <keghamngum@gmail.com>
2025-04-03 17:39:22 +0100

    Add simple_chat_marketplace/marketplace/index.php 1743698361932

--- simple_chat_marketplace/marketplace/index.php
+++ simple_chat_marketplace/marketplace/index.php
@@ -24,7 +24,7 @@ $products = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marketplace - ChatMarket</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body class="bg-gray-100">
